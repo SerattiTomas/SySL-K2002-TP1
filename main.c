624f85a1140b47bd1fe0e1a89c0408ded5a0d469
SerattiTomas <tomy.seratti@gmail.com>
2022-05-22 01:18:46 -0300

    agregado lectura de archivos txt y sistemas de opciones

--- main.c
+++ main.c
@@ -3,21 +3,9 @@
 
 // Tomás Seratti K2002
 
-int main(){
+void cadenaProcesarEImprimir(char cadena[], char cadenaProcesada[]){
 
-    int cantidadCaracteresMax = 10000;
-    char cadena[cantidadCaracteresMax];
-    char cadenaProcesada[cantidadCaracteresMax];
-
-    int i=0;
     int k=0;
-
-    printf("Ingrese una cadena de texto: \n");
-
-    while((cadena[i++] = getchar()) != EOF && i<cantidadCaracteresMax);
-    cadena[i-1] = '\0';
-
-    printf("\nTexto ingresado: \n%s", cadena);
 
     for (int j = 0; cadena[j]; j++){
 
@@ -48,7 +36,69 @@ int main(){
         k++;
     }
 
-    printf("\nCadena Procesada: \n%s", cadenaProcesada);
+    printf("\nCadena Procesada: %s", cadenaProcesada);
+
+    return;
+}
+
+int main(){
+
+    int cantidadCaracteresMax = 10000;
+    char cadena[cantidadCaracteresMax];
+    char cadenaProcesada[cantidadCaracteresMax];
+
+    int opcion;
+    printf("Si quiere ingresar el texto por consola, digite '1'\nSi quiere ingresarlo por archivo de texto, digite '2'\n");
+
+    scanf("%d", &opcion);
+
+    if(opcion == 1){
+
+        int i=0;
+
+        printf("Ingrese una cadena de texto: \n");
+
+        while((cadena[i++] = getchar()) != EOF && i<cantidadCaracteresMax);
+        cadena[i-1] = '\0';
+
+        cadenaProcesarEImprimir(cadena, cadenaProcesada);
+        return 1;
+    }
+
+    if(opcion == 2){
+
+        char ruta[cantidadCaracteresMax];
+        printf("Ingrese la ruta del archivo de texto:\n");
+
+        scanf("%s", &ruta);
+
+        FILE * cadenaTxt = fopen(ruta, "rb");
+        if (cadenaTxt == NULL){
+            perror("Error al abrir el archivo de texto");
+            return 1;
+        }
+
+        char caracter;
+        int l = 0;
+
+        while (feof(cadenaTxt)==0)
+        {
+            caracter = fgetc(cadenaTxt);
+            cadena[l] = caracter;
+            l++;
+        }
+
+        fclose(cadenaTxt);
+
+        cadenaProcesarEImprimir(cadena, cadenaProcesada);
+        return 1;
+    }
+
+    if(opcion != 1 && opcion != 2){
+        printf("\nDebe elegir una opcion valida\n");
+        main();
+        return 1;
+    }
 
     return 0;
 };
